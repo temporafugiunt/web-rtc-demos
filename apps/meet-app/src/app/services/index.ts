7d6eee502c256711f63e955c408e7561ff116c7f
James Eby <admin@freebytech.com>
2020-10-27 16:42:50 -0500

    Attempting to fix the OIDC configuration issues dynamically based upon analysis of origin and baseUrl in combination.

--- apps/meet-app/src/app/services/index.ts
+++ apps/meet-app/src/app/services/index.ts
@@ -4,11 +4,11 @@ import { CalendarService } from '@webrtc-demos/google';
 
 const authSettings: AuthEnvironmentModel = {
   stsServer: environment.stsServer,
-  redirectUrl: window.location.origin + '/redirect',
+  redirectUrl: '/redirect', // Needs full proper redirect here, but will be done with the Location service
   clientId: environment.clientId,
   responseType: 'id_token token', // 'code',
   scope: 'openid email profile',
-  postLogoutRedirectUri: window.location.origin + '/info',
+  postLogoutRedirectUri: '/info', // Needs full proper redirect here, but will be done with the Location service
   postLoginRoute: '/projects',
   forbiddenRoute: '/forbidden',
   unauthorizedRoute: '/unauthorized',
--- libs/auth/src/lib/auth-init.service.ts
+++ libs/auth/src/lib/auth-init.service.ts
@@ -1,4 +1,5 @@
 import { Inject, Injectable, isDevMode } from '@angular/core';
+import { PlatformLocation, Location } from '@angular/common';
 import { OidcConfigService, LogLevel } from 'angular-auth-oidc-client';
 import {
   AuthEnvironmentModel,
@@ -12,7 +13,9 @@ export class AuthInitService {
   constructor(
     @Inject(AUTH_ENVIRONMENT)
     private authEnvironmentModel: AuthEnvironmentModel,
-    private oidcConfigService: OidcConfigService
+    private oidcConfigService: OidcConfigService,
+    private platformLocation: PlatformLocation,
+    private location: Location
   ) {}
 
   public configureAuth(): Promise<any> {
@@ -26,8 +29,8 @@ export class AuthInitService {
 
     return this.oidcConfigService.withConfig({
       stsServer: this.authEnvironmentModel.stsServer,
-      redirectUrl: this.authEnvironmentModel.redirectUrl,
-      postLogoutRedirectUri: this.authEnvironmentModel.postLogoutRedirectUri,
+      redirectUrl: (this.platformLocation as any).location.origin + this.location.prepareExternalUrl(this.authEnvironmentModel.redirectUrl),
+      postLogoutRedirectUri: (this.platformLocation as any).location.origin + this.location.prepareExternalUrl(this.authEnvironmentModel.postLogoutRedirectUri),
       clientId: this.authEnvironmentModel.clientId,
       scope: this.authEnvironmentModel.scope,
       responseType: this.authEnvironmentModel.responseType,
